1136531269bc29c3a959d7b7160231d68ea7c615
Marcin Domański <marcindomanski.pl@gmail.com>
2017-04-02 22:58:10 +0200

    current date fixed

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ import os
 import zipfile
 import csv
 import numpy
+from datetime import date
 
-CURRENT_YEAR = '2016'
-path_mst = ((os.path.join( os.path.curdir, 'mst')))
+CURRENT_YEAR = str(date.today().year)
+path_mst = (os.path.join( os.path.curdir, 'mst'))
+
 def get_data():
     url = 'http://bossa.pl/pub/metastock/mstock/mstall.zip'
+    print('getting...')
     req = requests.get(url)
+    print('success')
     stock_file = open(os.path.join('file', os.path.basename(url)), 'wb')
     for chunk in req.iter_content(100000):
         stock_file.write(chunk)
@@ -16,20 +20,19 @@ def get_data():
     zipf = zipfile.ZipFile(os.path.join('file', os.path.basename(url)))
     zipf.extractall((os.path.join( os.path.curdir, 'mst')))
 
-#get_data()
-#print(os.listdir((os.path.join( os.path.curdir, 'mst'))))
-files = [f for f in (os.listdir((os.path.join( os.path.curdir, 'mst')))) if os.path.isfile(os.path.join(path_mst, f))]
-#print(files)
+get_data()
+
+files = [f for f in (os.listdir((os.path.join(os.path.curdir, 'mst')))) if os.path.isfile(os.path.join(path_mst, f))]\
 
 for fi in files:
-    #if fi!='HYDROGD.mst':
-    #    continue
+
     f = open(os.path.join(path_mst, fi))
     cs = csv.reader(f)
     cs_list = list(cs)
-    date_of_last =(cs_list[len(cs_list)-1] [1])
-    if(date_of_last[:4])!=CURRENT_YEAR:
+    date_of_last = (cs_list[len(cs_list)-1][1])
+    if(date_of_last[:4] ) != CURRENT_YEAR:
         continue
+
     header = cs_list.pop(0) #removes header
     float_list = []
     ticker = ''
@@ -43,11 +46,11 @@ for fi in files:
     #print(closes)
     max_close = max(closes)
     last_close = float_list[len(float_list)-1]
-   # print(last_close)
+    #print(last_close)
     last_close = last_close[4]
     #print(max_close)
 
-    if last_close==max_close:
+    if last_close == max_close:
         print(ticker)
 
 
